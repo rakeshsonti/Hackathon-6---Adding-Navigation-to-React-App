388d8a7c7330a1ec532a743fec73f6a543203315
rambhajansonti <ramsonti0597@gmail.com>
2020-12-09 14:40:12 +0530

    fs

--- src/components/App.js
+++ src/components/App.js
@@ -9,14 +9,19 @@ import {
 import "../styles/App.css";
 import About from "./About/about";
 import Home from "./Home/home";
-import LocationDisplay from "./LocationDisplay";
+// import LocationDisplay from "./LocationDisplay";
 function BadRequest() {
   return <div>'No match'</div>;
+}
+function LocationDisplay(){
+  const obj=useLocation();
+return (<div ata-tdestid="location-display">{obj.pathname}</div>)
 }
 class App extends Component {
   constructor(props) {
     super(props);
   }
+
 
   render() {
     return (
@@ -26,11 +31,9 @@ class App extends Component {
           <Link to="/about">About</Link>
           <LocationDisplay />
           <Switch>
-            <Route exact path="/about" render={About}/>
-            <Route exact path="/" render={Home}/>
-            <Route>
-      <BadRequest />
-    </Route>
+            <Route exact path="/about" component={About} />
+            <Route exact path="/" component={Home} />
+            <Route component={BadRequest}></Route>
           </Switch>
         </BrowserRouter>
       </div>
--- src/components/LocationDisplay.js
+++ src/components/LocationDisplay.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { useLocation} from "react-router-dom";
 function LocationDisplay(){
     const obj=useLocation();
+    console.log('aaya');
 return (<div data-testid="location-display">{obj.pathname}</div>)
 }
 export default LocationDisplay;
+
