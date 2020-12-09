b6fb9c0f1ff5412166795a21281251189eeba542
rambhajansonti <ramsonti0597@gmail.com>
2020-12-09 14:27:02 +0530

    fs

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,41 @@
 import React, { Component, useState } from "react";
-import { useLocation,Link, Route, BrowserRouter  } from "react-router-dom";
+import {
+  Link,
+  Route,
+  BrowserRouter,
+  useLocation,
+  Switch,
+} from "react-router-dom";
 import "../styles/App.css";
 import About from "./About/about";
 import Home from "./Home/home";
-import LocationDisplay from './DisplayLocation';
+import LocationDisplay from "./LocationDisplay";
+function BadRequest() {
+  return <div>'No match'</div>;
+}
 class App extends Component {
   constructor(props) {
     super(props);
   }
- 
+
   render() {
     return (
       <div id="main">
-        {/* <Router>
-          <Link to="/">Home</Link>
-          <Link to="/about/">About</Link>
-          <Route path="/" exact component={Home}></Route>
-          <Route path="/about/" exact component={About}></Route>
-        </Router> */}
         <BrowserRouter>
-        <LocationDisplay/>
-        <Link to="/">Home</Link>
-          <Link to="/about/">About</Link>
-          <Route path="/" exact component={Home}></Route>
-          <Route path="/about/" exact component={About}></Route>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <LocationDisplay />
+          <Switch>
+            <Route exact path="/about" render={About}/>
+            <Route exact path="/" render={Home}/>
+            <Route>
+      <BadRequest />
+    </Route>
+          </Switch>
         </BrowserRouter>
       </div>
     );
   }
-  
 }
 
 export default App;
--- src/components/LocationDisplay.js
+++ src/components/LocationDisplay.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { useLocation,Link, Route, BrowserRouter  } from "react-router-dom";
+import { useLocation} from "react-router-dom";
 function LocationDisplay(){
     const obj=useLocation();
-    // console.log(pathname);
 return (<div data-testid="location-display">{obj.pathname}</div>)
 }
 export default LocationDisplay;
